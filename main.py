ad3dccd58db5a9797205fe5f8850ff56b4c2ddf3
Major-Zizoo <norelgndy@gmail.com>
2024-12-15 16:24:01 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+#Take input
+print("Half Pyramid Pattern of Stars(*):")
+n=int(input("Enter the number rows:"))
+for i in range(n):
+     for j in range(i+1):
+        print("*",end="")
+     print()
